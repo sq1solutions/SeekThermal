5219d7f1c606a7f04532a75e8a609a6412e69a9a
mrpatrickneiler <patrick@squareonesolutions.net>
2014-05-06 15:04:33 -0700

    Homepage edits

--- js/_main.js
+++ js/_main.js
@@ -68,9 +68,9 @@
 		var activeSlide = 1;
 		
 		// Build HTML for Nav
-		$('<div/>', {
-		    'id' : 'slideNav'
-		}).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">'+activeSlide+'</span>/<span id="maxSlides">'+slidesCount+'</span></li><li class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
+		//$('<div/>', {
+		 //   'id' : 'slideNav'
+		//}).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">'+activeSlide+'</span>/<span id="maxSlides">'+slidesCount+'</span></li><li class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
 		
 
 		// Navigation highligting
